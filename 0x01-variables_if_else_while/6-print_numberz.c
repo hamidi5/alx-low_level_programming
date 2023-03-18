85b2d0fd867a5e2d1ff92fc973e59c1bc415e5c2
hamidi5 <abdelilahhamidi4@gmail.com>
2023-03-17 18:59:28 -0700

    printnumbs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all single degits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}

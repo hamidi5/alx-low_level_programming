266a0e2dedf97a23ec98428399ea4918c8e5459f
hamidi5 <abdelilahhamidi4@gmail.com>
2023-03-19 18:55:42 -0700

    print_comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* main- this program will prints all possible
+ * different combinations of two digits
+ *
+ *returne: 0( correct  succesfully )
+*/
+int main ()
+
+{
+  putchar  x=01, y=10,  
+ 
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints all possible
- * combinations of single-digit numbers
+ * main -this program  Write a program
+ *  that prints
+ *  all possible combinations of single-digit numbers
  *
  * Return: Always 0 (Succes)
  */

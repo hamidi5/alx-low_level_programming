bc5aa1e2791ebf2a3292ded69902ad1ac864853a
hamidi5 <abdelilahhamidi4@gmail.com>
2023-04-06 19:12:49 -0700

    wildc

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
  * wildcmp - compares two strings and returns 1
  * if the strings can be considered identical,
  * otherwise return 0.
- * @s1: first string 
+ * @s1: first string
  * @s2: second string containing "*"
  *
  * Return: 1 if identical, else 0

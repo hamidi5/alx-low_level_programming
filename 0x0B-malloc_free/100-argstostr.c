d24e2446d6f2d3e3ecbb25677ace85d2572a97e1
hamidi5 <abdelilahhamidi4@gmail.com>
2023-04-12 00:34:05 -0700

    argo

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *argstostr - concatenates all arguments of the program.
+  *@ac: argument count.
+  *@av: pointer to array of size ac.
+  *Return: NULL if ac == 0 or av == null, Pointer to new string.
+  *NULL on fail.
+  */
+char *argstostr(int ac, char **av)
+{
+	int x, y, z, size;
+	char *arg;
+
+	size = 0;
+	z = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	x = 0;
+	while (x < ac)
+	{
+		y = 0;
+		while (av[x][y])
+		{
+			size++;
+			y++;
+		}
+		size++;
+		x++;
+	}
+	arg = malloc((sizeof(char) * size) + 1);
+	if (arg == NULL)
+		return (NULL);
+	x = 0;
+	while (x < ac)
+	{
+		y = 0;
+		while (av[x][y])
+		{
+			arg[z] = av[x][y];
+			y++;
+			z++;
+		}
+		arg[k] = '\n';
+		z++;
+		x++;
+	}
+	arg[z] = '\0';
+	return (arg);
+}

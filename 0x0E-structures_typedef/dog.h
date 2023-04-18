f11c2fbc289b7c9cf426552641bc2555e2e4c028
hamidi5 <abdelilahhamidi4@gmail.com>
2023-04-18 14:58:54 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a dowwg
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
